4346259cda1a093b1ca2ad875e9967f9b0ec9560
smjae96 <tlawodbs0528@naver.com>
2024-02-24 11:27:41 +0900

    0224 commit

---
+++ src/codewars_0224/Kata2.java
@@ -0,0 +1,8 @@
+package codewars_0224;
+
+public class Kata2 {
+
+	  public static String numberToString(int num) {
+		  return Integer.valueOf(num).toString();
+	  }
+}
--- src/main/main.java
+++ src/main/main.java
@@ -3,6 +3,7 @@ package main;
 import codewars_0221.sol.Kata;
 import codewars_0222.sol.Solution;
 import codewars_0223.sol.Vowels;
+import codewars_0224.sol.Kata2;
 
 public class main {
 
@@ -11,6 +12,7 @@ public class main {
 		Solution sol = new Solution();	//0222
 		System.out.println(sol.Solution(10));
 		System.out.println(Vowels.getCount("abcoudefg "));	//0223
+		System.out.println(Kata2.numberToString(123));
 	}
 
 }
